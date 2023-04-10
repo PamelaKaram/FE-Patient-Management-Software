7dcbcca84753e884c87fe5efef52ea99b3588dff
Tracy Salloum <tracy.salloum@lau.edu>
2023-04-10 21:06:55 +0300

    Add search bar component to Doctor page

--- src/pages/doctor.js
+++ src/pages/doctor.js
@@ -1,6 +1,10 @@
+import Searchbar from '../Components/finalSearchBar';
+import React from 'react';
+
 export default function Doctor() {
   return (
-    <>
-    </>
+    <div>
+      <Searchbar />
+    </div>
   );
-};
+}
