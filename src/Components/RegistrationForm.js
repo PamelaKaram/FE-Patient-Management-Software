9fec58004eae4a0ebf89fcfa08a4ccb5c2fbd412
Tracy Salloum <tracy.salloum@lau.edu>
2023-04-12 19:40:22 +0300

    This commit updates the RegisterPatient component to include a form that allows the user to input their personal information.

--- src/Components/RegistrationForm.js
+++ src/Components/RegistrationForm.js
@@ -19,3 +19,43 @@ const RegisterPatient = () => {
     console.log('Password:', password);
     console.log('Phone Number:', phone);
   };
+
+  return (
+    <form className={SideStyles.page} onSubmit={handleSubmit}>
+      <div className={SideStyles.header}>
+        <h2 className={SideStyles.title}>Register Patient</h2>
+      </div>
+      <label className={SideStyles.attribute}>
+        First Name:
+        <input className={SideStyles.text} type="text" value={firstName} onChange={(event) => setFirstName(event.target.value)} />
+      </label>
+      <br />
+      <label className={SideStyles.attribute}>
+        Last Name:
+        <input className={SideStyles.text} type="text" value={lastName} onChange={(event) => setLastName(event.target.value)} />
+      </label>
+      <br />
+      <label className={SideStyles.attribute}>
+        Age:
+        <input className={SideStyles.text} type="text" value={age} onChange={(event) => setAge(event.target.value)} />
+      </label>
+      <br />
+      <label className={SideStyles.attribute}>
+        Email:
+        <input className={SideStyles.text} type="text" value={email} onChange={(event) => setEmail(event.target.value)} />
+      </label>
+      <br />
+      <label className={SideStyles.attribute}>
+        Password:
+        <input className={SideStyles.text} type="password" value={password} onChange={(event) => setPassword(event.target.value)} />
+      </label>
+      <br />
+      <label className={SideStyles.attribute}>
+        Phone Number:
+        <input className={SideStyles.text} type="text" value={phone} onChange={(event) => setPhone(event.target.value)} />
+      </label>
+      <br />
+      <button className={SideStyles.button} type="submit">Register</button>
+    </form>
+  );
+};
