9f9a9040806ca207a7035346feefb2b9fffa29cb
Tracy Salloum <tracy.salloum@lau.edu>
2023-04-12 15:33:01 +0300

    temp commit

--- src/pages/doctor.js
+++ src/pages/doctor.js
@@ -1,6 +1,32 @@
+import Searchbar from '../Components/FinalSearchBar';
+import React from 'react';
+import ActivityButton from '../Components/ActivityButton';
+import { BrowserRouter, Link } from 'react-router-dom';
+import SideStyles from "../styles/DoctorPage.module.css"
+import Sidebar from '../Components/Sidenav1';
+
 export default function Doctor() {
   return (
-    <>
-    </>
+      <div className={SideStyles.body}>
+        <Sidebar/>
+        <div>
+          <Searchbar />
+          </div>
+        {/*<Link to="/patient_registration">
+          <ActivityButton name="Register Patient" path="/patient_registration" />
+        </Link> */}
+        <div className={SideStyles.container}>
+          <ul className={SideStyles.buttonContainer}>
+            <ActivityButton className={SideStyles.button} name="Register Patient" path="/patient_registration" />
+            <ActivityButton className={SideStyles.button} name="Patient Medical Condition" path="" />
+            <ActivityButton className={SideStyles.button} name="Patient Medical History" path="" />
+          </ul>
+          <ul className={SideStyles.buttonContainer}>
+            <ActivityButton className={SideStyles.button} name="Patient Medical Records & Tests" path="/patient_registration" />
+            <ActivityButton className={SideStyles.button} name="Patient changes in Medical Condition" path="" />
+            <ActivityButton className={SideStyles.button} name="Update Patient Medical Prescription" path="" />
+          </ul>
+        </div>
+      </div>
   );
 };
