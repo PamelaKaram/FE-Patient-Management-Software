d6ac700fb92edfa17162e33396a6b0cc8e2765dc
Tracy Salloum <tracy.salloum@lau.edu>
2023-04-12 19:58:33 +0300

    This commit adds code to create a Contact component in a React application. The Contact component contains a list of contact information items with icons.

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -6,3 +6,36 @@ import locationIcon from "../icons/locationIcon.svg";
 import timeIcon from "../icons/timeIcon.svg";
 import exclamationIcon from "../icons/exclamationIcon.svg";
 import Image from "next/image";
+
+export default function Contact() {
+    return (
+        <div className={SideStyles.container}>
+            <ul className={SideStyles.sets}>
+                <li className={SideStyles.item}>
+                    <Image 
+                    src={phoneIcon.src} 
+                    alt="Phone"  
+                    width={33}
+                    height={33}
+                    className={SideStyles.imageClass} />
+                    <p className={SideStyles.textClass}> 0X-XXX XXX</p>
+                </li>
+                <li className={SideStyles.item}>
+                    <Image 
+                    src={inboxIcon.src} 
+                    alt="Inbox"  
+                    width={33}
+                    height={33}
+                    className={SideStyles.imageClass} />
+                    <p className={SideStyles.textClass}> example@gmail.com</p>
+                </li>
+                <li className={SideStyles.item}>
+                    <Image 
+                    src={locationIcon.src} 
+                    alt="Location"  
+                    width={33}
+                    height={33}
+                    className={SideStyles.imageClass} />
+                    <p className={SideStyles.textClass}> Arz Hospital, Zalka, Lebanon </p>
+                </li>
+            </ul>
