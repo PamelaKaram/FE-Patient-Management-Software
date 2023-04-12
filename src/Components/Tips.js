425404d3fd4b2272ec6ce342eacc42e8ad6f2b33
Tracy Salloum <tracy.salloum@lau.edu>
2023-04-12 20:08:30 +0300

    Added initial code for Tips component including the container, title, and introduction sentence.

---
+++ src/Components/Tips.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SideStyles from '../styles/Tips.module.css';
+
+export default function Tips() {
+    return (
+        <div className={SideStyles.container}>
+            <h2 className={SideStyles.title}>
+                Overall Health Tips!
+            </h2>
+            <p className={SideStyles.introSentence}>
+                Here are some of the best health tips that you can follow to maintain good health:
+            </p>
