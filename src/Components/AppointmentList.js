b9727d3b47e567d6a085499e60b2ed79d136672a
PamelaKaram <pamela.karam01@lau.edu>
2023-04-13 23:15:08 +0300

    few changes in the preview of apt

--- src/Components/AppointmentList.js
+++ src/Components/AppointmentList.js
@@ -5,16 +5,16 @@ import cancelIcon from "../icons/cancelIcon.svg";
 import Image from "next/image";
 
 const appointmentsData = [
-  { id: 1, title: "Appointment 1", startTime: "10:00 AM", endTime: "11:00 AM" },
-  { id: 2, title: "Appointment 2", startTime: "11:00 AM", endTime: "12:00 PM" },
-  { id: 3, title: "Appointment 3", startTime: "12:00 PM", endTime: "1:00 PM" },
-  { id: 4, title: "Appointment 4", startTime: "1:00 PM", endTime: "2:00 PM" }
+  { id: 1, patient: "patient", startTime: "10:00 AM", endTime: "11:00 AM" },
+  { id: 2, patient: "patient", startTime: "11:00 AM", endTime: "12:00 PM" },
+  { id: 3, patient: "patient", startTime: "12:00 PM", endTime: "1:00 PM" },
+  { id: 4, patient: "patient", startTime: "1:00 PM", endTime: "2:00 PM" }
 ];
 
 const AppointmentsList = () => {
   const list = appointmentsData.map((appointment) => (
     <div key={appointment.id} className={appL.appointmentpreview}>
-      <div className={appL.previewtitle}>{appointment.title}</div>
+      <div className={appL.previewpatient}> Appointment <span>with</span> {appointment.patient}</div>
       <div className={appL.info}>
         <div className={appL.previewtime}>
           {appointment.startTime} - {appointment.endTime}
@@ -47,7 +47,6 @@ const AppointmentsList = () => {
     <div className={appL.appointmentscontainer}>
       {list}
       <div className={appL.decisions}>
-
         <button>
           view all
         </button>
