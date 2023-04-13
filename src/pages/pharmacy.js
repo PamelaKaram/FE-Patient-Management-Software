e8f8130e78dd273418d913d67ac21088bd20b979
Tracy Salloum <tracy.salloum@lau.edu>
2023-04-13 14:17:54 +0300

    fixing error

--- src/pages/pharmacy.js
+++ src/pages/pharmacy.js
@@ -12,7 +12,7 @@ export default function Pharmacy() {
             <div>
                 <div className={SideStyles.imageContainer}>
                     <div className={SideStyles.bottomCenter}>
-                        <SearchBar />
+                        <Searchbar />
                     </div>
                 </div>
             </div>
