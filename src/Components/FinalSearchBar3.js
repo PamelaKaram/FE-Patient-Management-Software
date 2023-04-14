c4e59a3b50bf5e37c915101ab55d9ec5ba618ad5
Tracy Salloum <tracy.salloum@lau.edu>
2023-04-15 01:16:50 +0300

    doctor's page final pharmacy search bar

---
+++ src/Components/FinalSearchBar3.js
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import SearchIcon from "../icons/coloredSearchIcon.svg";
+import SearchBar from './PharmSearchBar';
+import SearchResultsList from './SearchResultsList';
+import SideStyles from "../styles/finalSearchBar.module.css"
+
+export default function Searchbar() {
+  const [results, setResults] = useState([]);
+
+  return (
+    <div className={SideStyles.App}>
+      <div className={SideStyles.searchBarContainer}>
+        <SearchBar setResults={setResults} iconUrl={SearchIcon}/>
+        {results && results.length > 0 && <SearchResultsList results={results} />}
+      </div>
+    </div>
+  );
+}
