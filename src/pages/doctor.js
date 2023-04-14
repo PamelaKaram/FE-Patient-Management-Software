4ef982d78c665a83ab5eb6d2332cedeed4d905f8
PamelaKaram <pamela.karam01@lau.edu>
2023-04-14 12:40:48 +0300

    changed naming convention

--- src/pages/doctor.js
+++ src/pages/doctor.js
@@ -2,24 +2,29 @@ import Searchbar from '../Components/finalSearchBar';
 import React from 'react';
 import ActivityButton from '../Components/ActivityButton';
 import { BrowserRouter, Link } from 'react-router-dom';
-import SideStyles from "../styles/DoctorPage.module.css"
+import DoctorStyles from "../styles/DoctorPage.module.css"
 import Sidebar from '../Components/Sidenav1';
+import Welcome from "../Components/Welcome";
 
 export default function Doctor() {
   return (
-      <div className={SideStyles.body}>
+      <div className={DoctorStyles.body}>
         <Sidebar/>
         <div>
-          <div className={SideStyles.imageContainer}>
-            <div className={SideStyles.bottomCenter}>
+          <div className={DoctorStyles.imageContainer}>
+            <div className={DoctorStyles.bottomCenter}>
               <Searchbar />
             </div>
           </div>
         </div>
+        
+        <div className={DoctorStyles.header}>
+          <Welcome />
+        </div>
         {/*<Link to="/patient_registration">
           <ActivityButton name="Register Patient" path="/patient_registration" />
         </Link> */}
-        <div className={SideStyles.container}>
+        {/* <div className={SideStyles.container}>
           <ul className={SideStyles.buttonContainer}>
             <ActivityButton className={SideStyles.button} name="Register Patient" path="/patient_registration" />
             <ActivityButton className={SideStyles.button} name="Patient Medical Condition" path="" />
@@ -30,7 +35,7 @@ export default function Doctor() {
             <ActivityButton className={SideStyles.button} name="Patient changes in Medical Condition" path="" />
             <ActivityButton className={SideStyles.button} name="Update Patient Medical Prescription" path="" />
           </ul>
-        </div>
+        </div> */}
       </div>
   );
 }
