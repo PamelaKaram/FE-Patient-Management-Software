44f7c03aef4f4276c9041920fee53124f6a29d02
Tracy Salloum <tracy.salloum@lau.edu>
2023-04-16 15:27:17 +0300

    adding components to the patient's view

--- src/pages/patient.js
+++ src/pages/patient.js
@@ -1,16 +1,27 @@
 import React from 'react';
-import SideStyles from '../styles/PatientPage.module.css'
-import NavBar from '../Components/Sidenav4'
-import SideBar from '../Components/SideBar'
+import SideStyles from '../styles/PatientPage.module.css';
+import NavBar from '../Components/Sidenav4';
+import SideBar from '../Components/SideBar';
+import AppDateTime from '../Components/AppDateTime';
 
 export default function Doctor() {
     return (
         <div className={SideStyles.body}>
           <NavBar/>
           <SideBar />
+          <div className={SideStyles.leftHalf}>
+            <div className={SideStyles.header}>
+              <h1>Hi, Pamela Karam</h1>
+              <p className={SideStyles.text}>Let's track your health Today!</p>
+            </div>
+            <div className={SideStyles.app}>
+              <h3>Upcoming Appointments</h3>
+              <AppDateTime />
+            </div>
+          </div>
         </div>
 
-        );
-    }
+    );
+}
 
 
