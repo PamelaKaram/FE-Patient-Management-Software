a1257345d3b3126f1de157e821d76a620cdcb035
Tracy Salloum <tracy.salloum@lau.edu>
2023-04-17 20:01:49 +0300

    adding components to the patient's view

--- src/pages/patient.js
+++ src/pages/patient.js
@@ -3,6 +3,9 @@ import SideStyles from '../styles/PatientPage.module.css';
 import NavBar from '../Components/Sidenav4';
 import SideBar from '../Components/SideBar';
 import AppDateTime from '../Components/AppDateTime';
+import Buttons from '../Components/Buttons';
+import Reminders from '../Components/Reminders';
+import Links from '../Components/Links';
 
 export default function Doctor() {
     return (
@@ -17,6 +20,23 @@ export default function Doctor() {
             <div className={SideStyles.app}>
               <h3>Upcoming Appointments</h3>
               <AppDateTime />
+            </div>
+            <div className={SideStyles.activities}>
+              <h3>Activities</h3>
+              <p className={SideStyles.text}>Today, 7 May 2023</p>
+              <Buttons/>
+            </div>
+            <div className={SideStyles.bottom}>
+              <div className={SideStyles.reminder}> 
+                  <h3>Daily Reminder!</h3>
+                  <Reminders />
+              </div>
+              <div className={SideStyles.link}>
+                  <div>
+                    <h3>Quick Links</h3>
+                    <Links />
+                  </div>
+              </div>
             </div>
           </div>
         </div>
