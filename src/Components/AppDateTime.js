537c8ea36b4be8a12e094823dbf05e172e0269ca
Tracy Salloum <tracy.salloum@lau.edu>
2023-04-17 19:51:59 +0300

    adding more elements to the patient's appointment date and time

--- src/Components/AppDateTime.js
+++ src/Components/AppDateTime.js
@@ -2,6 +2,9 @@ import React from "react";
 import ClinicIcon from '../icons/clinicIcon.svg';
 import SideStyles from '../styles/AppDateTime.module.css';
 import Image from 'next/image';
+import CalendarIcon from '../icons/blueCalendarIcon.svg';
+import TimeIcon from '../icons/blueClockIcon.svg';
+
 
 const AppDateTime = () => {
     return (
@@ -18,6 +21,25 @@ const AppDateTime = () => {
                     <p className={SideStyles.details}>Jal El Dib, Lebanon</p>
                 </div>
             </div>
+            <div className={SideStyles.dateTime}>
+                <div className={SideStyles.corres1}>
+                    <Image 
+                    src={CalendarIcon.src} 
+                    alt="Calendar" 
+                    width={50}
+                    height={50}
+                    className={SideStyles.infoImage}/>
+                    <p><b>14 May 2023</b></p>
+                </div>
+                <div className={SideStyles.corres2}>
+                    <Image 
+                    src={TimeIcon.src} 
+                    alt="time" 
+                    width={50}
+                    height={50}
+                    className={SideStyles.infoImage}/>
+                    <p><b>09.00 am</b></p>
+                </div>
             </div>
         </div>
     );
