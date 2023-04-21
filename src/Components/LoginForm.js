f6e6ef4d07dfbf71db3d7d1ff1f5d5b6d442ece0
PamelaKaram <pamela.karam01@lau.edu>
2023-04-22 01:22:29 +0300

    added css styles to the page - login

--- src/Components/LoginForm.js
+++ src/Components/LoginForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import LoginStyles from '../styles/login.module.css';
 
 export default function LoginForm({ Login, error}) {
     const [details, setDetails] = useState({name:"",email:"",password:""});
@@ -11,18 +12,18 @@ export default function LoginForm({ Login, error}) {
 
   return (
     <form onSubmit={submitHandler}>
-        <div className="form-inner">
+        <div className={LoginStyles.formInner}>
             <h2>Login</h2>
-            {(error != ""  )? ( <div className="error">{error}</div> ) : ""}
-            <div className="form-group">
+            {(error != ""  )? ( <div className={LoginStyles.error}>{error}</div> ) : ""}
+            <div className={LoginStyles.formGroup}>
                 <label htmlFor="name">Name: </label>
                 <input type="text" name="name" id="name" onChange={e => setDetails({...details, name: e.target.value }) } value={details.name}/>
             </div>
-            <div className="form-group">
+            <div className={LoginStyles.formGroup}>
                 <label htmlFor="email">Email: </label>
                 <input type="email" name="email" id="email" onChange={e => setDetails({...details, email: e.target.value }) } value={details.email} />
             </div>
-            <div className="form-group">
+            <div className={LoginStyles.formGroup}>
                 <label htmlFor="password">Password: </label>
                 <input type="password" name="password" id="password" onChange={e => setDetails({...details, password: e.target.value }) } value={details.password}/>
             </div>
