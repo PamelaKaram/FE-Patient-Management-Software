18dfdad54bd6dcb5188a46596a633a44ef03b191
PamelaKaram <pamela.karam01@lau.edu>
2023-04-22 14:20:19 +0300

    login to react hook form in home step 1

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,71 +4,15 @@ import Sidebar from '../Components/Sidenav1';
 // import AppointmentsList from '../Components/AppointmentList';
 import LoginForm from '../Components/LoginForm';
 import React, { useState } from 'react';
-import LoginStyles from '@/styles/login.module.css';
-
 
 
 export default function Home() {
-  const adminUser={
-    email:"admin@admin.com",
-    password:"admin123"
 
-  }
-  const [user,setUser]=useState({name:"",email:""});
-  const [error,setError]=useState("");
-  
-  const Login=details=>{
-    console.log(details);
-    if(details.email==adminUser.email && details.password==adminUser.password){
-      console.log("Logged in");
-      setUser({
-        name:details.name,
-        email:details.email
-      });
-    }else{
-      console.log("Details do not match!");
-      setError("Details do not match!");
-    }
-  }
-
-    const Logout = () =>{ 
-      console.log("Logout");
-       setUser({name:"",email:""});
-    }
-  
-    function validateUsername(username) {
-      if (username.length < 4 || username.length > 20) {
-        return false;
-      }
-      return true;
-    }
-
-    function validatePassword(password) {
-      var re = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}$/;
-      return re.test(password);
-    }
-
+  return (
+      <main className="login">
+        <LoginForm/>
+      </main> 
     
-
-
-  return ( 
-    
-    
-     <div className={LoginStyles.App}> 
-      {(user.email!="" ) ? (
-        <div className="welcome">
-          <h2>Welcome, <span>{user.name}</span></h2>
-          <button onClick={Logout}>Logout</button>
-        </div>
-        
-      ): ( 
-      <LoginForm Login={Login} error={error} />
-      )}   
-
-     {/* <main className="test">
-        <Sidebar/>
-      </main> */}
-    </div>
   )
 }
 
