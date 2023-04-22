d6673445211b3061f6f3d601e904b40ef2c7a012
PamelaKaram <pamela.karam01@lau.edu>
2023-04-22 23:41:55 +0300

    edited chatActivity added chat list

--- src/Components/ChatActivity.js
+++ src/Components/ChatActivity.js
@@ -1,70 +1,62 @@
-import appL from '../styles/AppointmentList.module.css';
+import ChatActivity from '../styles/ChatActivity.module.css';
 import React from "react";
 import doneIcon from "../icons/doneIcon.svg";
 import cancelIcon from "../icons/cancelIcon.svg";
 import Image from "next/image";
 import WelcomeStyles from "../styles/Welcome.module.css";
-
+import { Avatar } from '@mui/material';
+// import ChatActivity from '../styles/ChatActivity.module.css';
 const appointmentsData = [
-  { id: 1, patient: "patient", startTime: "10:00 AM", endTime: "11:00 AM" },
-  { id: 2, patient: "patient", startTime: "11:00 AM", endTime: "12:00 PM" },
-  { id: 3, patient: "patient", startTime: "12:00 PM", endTime: "1:00 PM" },
-  { id: 4, patient: "patient", startTime: "1:00 PM", endTime: "2:00 PM" }
+    { id: 1, patient: "Jad Abou Karam" },
+    { id: 2, patient: "Habib El Deek" },
+    { id: 3, patient: "Habib El Deek" },
+    { id: 4, patient: "Tracy Salloum" }
 ];
-
+//note redirect to wpp
 const AppointmentsList = () => {
-  
-  const list = appointmentsData.map((appointment) => (
-    <div key={appointment.id} className={appL.appointmentpreview}>
-      <div className={appL.previewpatient}> Appointment <span>with</span> {appointment.patient}</div>
-      <div className={appL.info}>
-        <div className={appL.previewtime}>
-          {appointment.startTime} - {appointment.endTime}
-        </div>
-        <div className={appL.buttons}>
-          <ul>
-            <li>
-              <Image
-                src={doneIcon.src}
-                alt="done"
-                width={25}
-                height={25}
-              />
-            </li>
-            <li>
-              <Image
-                src={cancelIcon.src}
-                alt="done"
-                width={25}
-                height={25}
-              />
-            </li>
-          </ul>
-        </div>
-      </div>
 
-    </div>
-  ))
-  return (
-    <div>
-      <div className={WelcomeStyles.main}>
-        <h1>Looks like you've got some questions to answer </h1>
-      </div>
-    
-    <div className={appL.appointmentscontainer}>
-      {list}
-      <div className={appL.decisions}>
-        <button>
-          view all
-        </button>
-        <button>
-          view future appointments
-        </button>
-      </div>
-    </div>
-    
-    </div>
-  );
+    const list = appointmentsData.map((appointment) => (
+        <div key={appointment.id} className={ChatActivity.appointmentpreview}>
+            <div className={ChatActivity.avatar}> 
+                <Avatar
+                    alt={appointment.patient}
+                    src="/static/images/avatar/1.jpg"
+                />
+            </div>
+            <div className={ChatActivity.previewpatient}> {appointment.patient}</div>
+            <div className={ChatActivity.info}>
+                <div className={ChatActivity.previewtime}>
+                    Lorem epsum dolor sit amet,
+                    consectetur adipiscing elit,
+                    sed do eiusmod tempor incididunt ut
+                    labore et dolore magna aliqua. amco ln proident
+
+                </div>
+                <div className={ChatActivity.decisions}>
+                    <button className={ChatActivity.color1}>
+                        Answer
+                    </button>
+                    <button className={ChatActivity.color2}>
+                        Call
+                    </button>
+                </div>
+            </div>
+
+        </div>
+    ))
+    return (
+        <div>
+            <div className={WelcomeStyles.main}>
+                <h1>Looks like you've got some questions to answer </h1>
+
+            </div>
+
+            <div className={ChatActivity.appointmentscontainer}>
+                {list}
+            </div>
+
+        </div>
+    );
 };
 
 export default AppointmentsList;
