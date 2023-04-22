82c39db70e99419ce165b41aa75597bfe3c5c2a8
PamelaKaram <pamela.karam01@lau.edu>
2023-04-23 00:30:55 +0300

    commented out home for testing of side nav resp

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,16 +3,16 @@ import styles from '@/styles/Home.module.css'
 import Sidebar from '../Components/Sidenav1';
 // import AppointmentsList from '../Components/AppointmentList';
 import LoginForm from '../Components/LoginForm';
-
+import Sidenav from '../Components/SideNavResponsive'
 
 
 export default function Home() {
 
   return (
-    <main className="login">
-      <LoginForm />
-    </main>
-
+    // <main className="login">
+    //   <LoginForm />
+    // </main>
+    <Sidenav/>
   )
 }
 
