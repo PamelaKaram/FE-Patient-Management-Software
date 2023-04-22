1628f33894ab6c8df4cd2c1444219bdbdf550b7a
PamelaKaram <pamela.karam01@lau.edu>
2023-04-22 22:28:54 +0300

    added quick links component

--- src/pages/doctor.js
+++ src/pages/doctor.js
@@ -7,34 +7,51 @@ import Sidebar from '../Components/Sidenav1';
 import Welcome from "../Components/Welcome";
 import AppointmentsList from '../Components/AppointmentList';
 import SideStyles from "../styles/DoctorActivityPage.module.css";
-import {Routes, Route, useNavigate} from 'react-router-dom';
-
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import Links from '../Components/Links';
+import LinksStyles from '../styles/Links.module.css';
+import  ChatActivty from '../Components/ChatActivity';
 function Doctor() {
-  
+
   return (
-      <div className={DoctorStyles.body}>
-        <Sidebar/>
-        <div>
-          <div className={DoctorStyles.imageContainer}>
-            <div className={DoctorStyles.bottomCenter}>
-              <Searchbar />
-            </div>
+    <div className={DoctorStyles.body}>
+      <Sidebar />
+      <div>
+        <div className={DoctorStyles.imageContainer}>
+          <div className={DoctorStyles.bottomCenter}>
+            <Searchbar />
           </div>
         </div>
-        
-        <div className={DoctorStyles.header}>
-          <Welcome />
-          <div className={AppointmentsList.apt}>
-            <AppointmentsList />
-          </div>
-        </div>
-        <button className={DoctorStyles.customButton} onClick={() => { window.location.href = "/patient_registration" }}>
-          Register Patient
-        </button>
-        <button className={DoctorStyles.customButton} onClick={() => { window.location.href = "/pharmacy_registration" }}>
-          Add Pharmacy
-        </button>
       </div>
+
+      <div className={DoctorStyles.header}>
+        <Welcome />
+        <div className={AppointmentsList.apt}>
+          <AppointmentsList />
+        </div>
+        <div className={LinksStyles.line}>
+          <h1> Quick Links </h1>
+          <Links />
+        </div>
+      </div>
+      
+      <div className={DoctorStyles.body}>
+      {/* <Sidebar /> */}
+      <div className={AppointmentsList.apt}>
+          <ChatActivty />
+        </div>
+      
+       
+      </div>
+
+
+      <button className={DoctorStyles.customButton} onClick={() => { window.location.href = "/patient_registration" }}>
+        Register Patient
+      </button>
+      <button className={DoctorStyles.customButton} onClick={() => { window.location.href = "/pharmacy_registration" }}>
+        Add Pharmacy
+      </button>
+    </div>
   );
 }
 
