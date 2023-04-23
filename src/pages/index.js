bb987e6ec617919fc61dc820a2b3fcb55e2a24e7
PamelaKaram <pamela.karam01@lau.edu>
2023-04-23 18:36:53 +0300

    rendering of sidenav on home page based on width

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,17 +3,41 @@ import styles from '@/styles/Home.module.css'
 import Sidebar from '../Components/Sidenav1';
 // import AppointmentsList from '../Components/AppointmentList';
 import LoginForm from '../Components/LoginForm';
-import Sidenav from '../Components/SideNavResponsive'
-
+import SidenavMobile from '../Components/SideNavResponsive'
+import SidenavWeb from '../Components/Sidenav2'
+import { useState, useEffect } from 'react';
 
 export default function Home() {
+// render the SidnavMobile component if the screen width is less than 768px
+// render the SidenavWeb component if the screen width is greater than 768px
+const [width, setWidth] = useState(0);
+const [height, setHeight] = useState(0);
 
-  return (
-    // <main className="login">
-    //   <LoginForm />
-    // </main>
-    <Sidenav/>
-  )
+const updateDimensions = () => {
+  setWidth(window.innerWidth);
+  setHeight(window.innerHeight);
+};
+
+useEffect(() => {
+  window.addEventListener("resize", updateDimensions);
+  return () => window.removeEventListener("resize", updateDimensions);
+}, []);
+return(
+ <main>
+  {width < 768 ? <SidenavMobile /> : <SidenavWeb />}
+  <LoginForm />
+
+  </main>
+  
+
+
+
+  
+ 
+
+)
+
+
+  
+  
 }
-
-
