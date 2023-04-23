9678671bbf4b197b92e1a9fb97867d7438a08086
PamelaKaram <pamela.karam01@lau.edu>
2023-04-23 18:36:10 +0300

    customized the side nav

--- src/Components/SideNavResponsive.js
+++ src/Components/SideNavResponsive.js
@@ -8,14 +8,21 @@ import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
+import { useTheme } from '@mui/material/styles';
+
 import menu from "../icons/SideNav.svg"
 import Image from "next/image";
+import SideStyles from "../styles/Sidenav3.module.css";
+import DashboardIcon from "../icons/dashboardIcon.svg";
+import SettingIcon from "../icons/settingsIcon.svg"
+import LogoutIcon from "../icons/logoutIcon.svg";
+import CalendarIcon from "../icons/calendarIcon.svg";
+import ChatIcon from "../icons/chatIcon.svg";
+import SearchIcon from "../icons/searchIcon.svg";
 export default function TemporaryDrawer() {
   const [state, setState] = React.useState({
     left: false,
-   
+
   });
 
   const toggleDrawer = (anchor, open) => (event) => {
@@ -25,77 +32,123 @@ export default function TemporaryDrawer() {
 
     setState({ ...state, [anchor]: open });
   };
-
+ 
+ 
   const list = (anchor) => (
+
+    
     <Box
-      sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250, backgroundColor: "#91BF76" }}
-      role="presentation"
+      sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250, height: '100vh'}}
       onClick={toggleDrawer(anchor, false)}
       onKeyDown={toggleDrawer(anchor, false)}
     >
+     
       <List>
-        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
+        
+        <ListItemButton>
+          <ListItemIcon>
+            <Image
+              src={DashboardIcon.src}
+              alt="Dashboard"
+              width={25}
+              height={25}
+            />
+          </ListItemIcon>
+          <ListItemText 
+         
+          primary={"Dashboard"} />
+        </ListItemButton>
+
+        <ListItemButton>
+          <ListItemIcon>
+            <Image
+              src={CalendarIcon.src}
+              alt="Calendar"
+              width={27}
+              height={27}
+            />
+          </ListItemIcon>
+          <ListItemText primary={"Calendar"} />
+        </ListItemButton>
+
+        <ListItemButton>
+          <ListItemIcon>
+            <Image
+              src={ChatIcon.src}
+              alt="Chat"
+              width={27}
+              height={27}
+            />
+          </ListItemIcon>
+          <ListItemText primary={"Chats"} />
+        </ListItemButton>
+
+        <ListItemButton>
+          <ListItemIcon>
+            <Image
+              src={SearchIcon.src}
+              alt="Dashboard"
+              width={27}
+              height={27}
+            />
+          </ListItemIcon>
+          <ListItemText primary={"Search"} />
+        </ListItemButton>
+
+        <ListItemButton>
+          <ListItemIcon>
+            <Image
+              src={SettingIcon.src}
+              alt="Settings"
+              width={27}
+              height={27}
+            />
+          </ListItemIcon>
+          <ListItemText primary={"Settings"} />
+        </ListItemButton>
+
       </List>
-      <Divider />
-      <List>
+ 
+      <Divider sx={{color: "#EDF8EB" }}/>
+      <ListItemButton>
+          <ListItemIcon>
+            <Image
+              src={LogoutIcon.src}
+              alt="Logout"
+              width={27}
+              height={27}
+            />
+          </ListItemIcon>
+          <ListItemText primary={"Logout"} />
+        </ListItemButton>
        
-          <ListItem key={"text"} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-             
-              </ListItemIcon>
-              <ListItemText primary={"text"} />
-            </ListItemButton>
-          </ListItem>
-
-          <ListItem key={"thtd"} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-              
-              </ListItemIcon>
-              <ListItemText primary={"text"} />
-            </ListItemButton>
-          </ListItem>
-            
-
-
-       
-
-      </List>
     </Box>
   );
 
+
   return (
     <div>
-        <React.Fragment>
-          <Button onClick={toggleDrawer("left", true)}    >
+       <Box>
+        <Button onClick={toggleDrawer("left", true)}    >
           <Image
-                src={menu.src}
-                alt="done"
-                width={25}
-                height={25}
-              />
+            src={menu.src}
+            alt="done"
+            width={25}
+            height={25}
+          />
 
-          </Button>
-          
-          <Drawer
-            anchor={"left"}
-            open={state["left"]}
-            onClose={toggleDrawer("left", false)}
-          >
-            {list("left")}
-          </Drawer>
-        </React.Fragment>
-      
+        </Button>
+
+        <Drawer
+          sx={{bgcolor:"#E9F3E8"}}
+          anchor={"left"}
+          open={state["left"]}
+          onClose={toggleDrawer("left", false)}
+        >
+          {list("left")}
+        </Drawer>
+        </Box>
+
     </div>
   );
-}
+  }
