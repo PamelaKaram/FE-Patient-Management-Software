3ecfcbaa37200519a5f153b2afec5241917c888b
Tracy Salloum <tracy.salloum@lau.edu>
2023-04-24 14:44:18 +0300

    modifying the hits container to be clickable

--- src/Components/hitsContainer.js
+++ src/Components/hitsContainer.js
@@ -1,14 +1,16 @@
 import { connectHits } from "react-instantsearch-dom";
+import Link from "next/link";
+import styles from "./HitsContainer.module.css";
 
 function HitsContainer({ hits }) {
   console.log(hits);
   return (
-    <div>
+    <div className={styles.container}>
       {hits.map((hit) => (
-        <div key={hit.objectID}>
-          <h2>
-            {hit.firstName} {hit.lastName}
-          </h2>
+        <div key={hit.objectID} className={styles.hit}>
+          <Link href={`/patient/\${hit.objectID}`}>
+            <a>{hit.firstName} {hit.lastName}</a>
+          </Link>
         </div>
       ))}
     </div>
