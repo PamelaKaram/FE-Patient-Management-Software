95870d43c26d5809064300839d2acb5544eaaa6c
PamelaKaram <pamela.karam01@lau.edu>
2023-04-27 21:44:17 +0300

    deleted css from component

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import Image from 'next/image';
 import back from '../icons/drImage.svg';
+import homeStyles from '../styles/Home.module.css';
 function About() {
   return (
-    <div style={{ backgroundColor: '#E8F1FF', display: 'flex' }}>
+    <div className={homeStyles.rectangle}>
       <Image 
-      src={back.src} alt="About section image" width="400" height="400" />
+      src={back.src} alt="About section image" width="400" height="400"
+      className={homeStyles.pic}/>
       <div>
         <h2>About Us</h2>
-        <p>We are a team of professionals dedicated to providing high-quality services to our clients.</p>
+        <p>My mission is to promote heart health and prevent heart disease in my patients. 
+            I offer a range of preventive measures such as lifestyle modifications, medication 
+            management, and regular monitoring to help my patients maintain optimal heart health.
+            Book a consultation, get diagnosed and find the best treatment.</p>
         <button onClick={() => alert('Request an appointment')}>Request an appointment</button>
       </div>
     </div>
