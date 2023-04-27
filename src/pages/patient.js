b521952dbd3b315fc8ead9b2be67853acc02fb72
PamelaKaram <pamela.karam01@lau.edu>
2023-04-27 13:58:35 +0300

    formatted doc

--- src/pages/patient.js
+++ src/pages/patient.js
@@ -18,57 +18,57 @@ export default function Doctor() {
   const handleSingleDateChange = (date) => {
     setDate(date);
   };
-    return (
-        <div className={SideStyles.body}>
-          <NavBar/>
-          <SideBar />
-          <div className={SideStyles.leftHalf}>
-            <div className={SideStyles.header}>
-              <h1>Hi, Pamela Karam</h1>
-              <p className={SideStyles.text}>Let's track your health Today!</p>
-            </div>
-            <div className={SideStyles.app}>
-              <h3>Upcoming Appointments</h3>
-              <AppDateTime />
-            </div>
-            <div className={SideStyles.activities}>
-              <h3>Activities</h3>
-              <p className={SideStyles.text}>Today, 7 May 2023</p>
-              <Buttons/>
-            </div>
-            <div className={SideStyles.bottom}>
-              <div className={SideStyles.reminder}> 
-                  <h3>Daily Reminder!</h3>
-                  <Reminders />
-              </div>
-              <div className={SideStyles.link}>
-                  <div>
-                    <h3>Quick Links</h3>
-                    <Links />
-                  </div>
-              </div>
+  return (
+    <div className={SideStyles.body}>
+      <NavBar />
+      <SideBar />
+      <div className={SideStyles.leftHalf}>
+        <div className={SideStyles.header}>
+          <h1>Hi, Pamela Karam</h1>
+          <p className={SideStyles.text}>Let's track your health Today!</p>
+        </div>
+        <div className={SideStyles.app}>
+          <h3>Upcoming Appointments</h3>
+          <AppDateTime />
+        </div>
+        <div className={SideStyles.activities}>
+          <h3>Activities</h3>
+          <p className={SideStyles.text}>Today, 7 May 2023</p>
+          <Buttons />
+        </div>
+        <div className={SideStyles.bottom}>
+          <div className={SideStyles.reminder}>
+            <h3>Daily Reminder!</h3>
+            <Reminders />
+          </div>
+          <div className={SideStyles.link}>
+            <div>
+              <h3>Quick Links</h3>
+              <Links />
             </div>
           </div>
-          <div style={{ width: "100%", margin: "50px" }}>
+        </div>
+      </div>
+      <div style={{ width: "100%", margin: "50px" }}>
         <LocalizationProvider dateAdapter={AdapterDateFns}>
           <StaticDatePicker
             value={date}
             label="See Appointments"
             onChange={handleSingleDateChange}
 
-            // initialVisibleMonth={() => moment()}
-            // focused={focus}
-            // numberOfMonths={1}
-            // onFocusChange={({ focused }) => setFocus(focused)}
-            // isDayBlocked={isBlocked}
+          // initialVisibleMonth={() => moment()}
+          // focused={focus}
+          // numberOfMonths={1}
+          // onFocusChange={({ focused }) => setFocus(focused)}
+          // isDayBlocked={isBlocked}
           />
         </LocalizationProvider>
       </div>
 
-        </div>
-        
+    </div>
 
-    );
+
+  );
 }
 
 
