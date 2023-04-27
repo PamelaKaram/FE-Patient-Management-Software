9bd4e0945789baad07a2b6d49d765d8aaaf66178
PamelaKaram <pamela.karam01@lau.edu>
2023-04-27 23:03:16 +0300

    added popup option to the requestApt

--- src/Components/About.js
+++ src/Components/About.js
@@ -2,22 +2,30 @@ import React from 'react';
 import Image from 'next/image';
 import back from '../icons/drImage.svg';
 import homeStyles from '../styles/Home.module.css';
+import ChatButton from '../Components/RequestAptPop';
+
+
+
+
+
+
 function About() {
-  return (
-    <div className={homeStyles.rectangle}>
-      <Image 
-      src={back.src} alt="About section image" width="400" height="400"
-      className={homeStyles.pic}/>
-      <div>
-        <h2>About Me</h2>
-        <p>My mission is to promote heart health and prevent heart disease in my patients. 
-            I offer a range of preventive measures such as lifestyle modifications, medication 
-            management, and regular monitoring to help my patients maintain optimal heart health.
-            Book a consultation, get diagnosed and find the best treatment.</p>
-        <button onClick={() => alert('Request an appointment')}>Request an appointment</button>
-      </div>
-    </div>
-  );
+    return (
+        <div className={homeStyles.rectangle}>
+            <Image
+                src={back.src} alt="About section image" width="400" height="400"
+                className={homeStyles.pic} />
+            <div className={homeStyles.about}>
+                <h2>About Me</h2>
+                <p>My mission is to promote heart health and prevent heart disease in my patients.
+                    I offer a range of preventive measures such as lifestyle modifications, medication
+                    management, and regular monitoring to help my patients maintain optimal heart health.
+                    Book a consultation, get diagnosed and find the best treatment.</p>
+                <ChatButton onClick={() => alert('Request an appointment')}
+                />
+            </div>
+        </div>
+    );
 }
 
 export default About;
