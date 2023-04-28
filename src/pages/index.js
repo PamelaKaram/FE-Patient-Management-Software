436f18bb08fa8cf7f49ade5ad29d80723908aa9f
PamelaKaram <pamela.karam01@lau.edu>
2023-04-29 00:22:05 +0300

    commented out footer because it was not helping with the responsiveness

--- src/pages/index.js
+++ src/pages/index.js
@@ -61,7 +61,7 @@ export default function Home() {
       <div>
         <BlogArticle articles={articles} />
         <Tips />
-        <Contact />
+        {/* <Contact /> */}
       </div>
     </main>
   );
