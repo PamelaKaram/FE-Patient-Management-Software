16bcfeca3f923f95a33f2abb380e421346a72cd5
Tracy Salloum <tracy.salloum@lau.edu>
2023-04-29 18:36:30 +0300

    calendar component modifications

--- src/Components/DateRangePicker.js
+++ src/Components/DateRangePicker.js
@@ -1,101 +1,52 @@
-import React, { useState } from "react";
-import { DateRangePicker } from "react-date-range";
-import "react-date-range/dist/styles.css"; // main style file
-import "react-date-range/dist/theme/default.css"; // theme css file
-import { LocalizationProvider } from "@mui/lab";
-import AdapterDateFns from "@mui/lab/AdapterDateFns";
-import TextField from "@mui/material/TextField";
-import moment from "moment";
+import React, { useState } from 'react';
+import { StaticDateRangePicker, DateRangePickerDay } from '@mui/lab';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import { styled } from '@mui/system';
 
-function DateRange() {
-    const [date, setDate] = useState(new Date());
-    const [focus, setFocus] = useState(false);
-  
-    const [dateRange, setDateRange] = useState({
-      startDate: new Date(),
-      endDate: null,
-      key: "selection",
-    });
+const StyledDateRangePickerDay = styled(DateRangePickerDay)(({ theme }) => ({
+  borderRadius: '50%',
+  '&:hover': {
+    backgroundColor: theme.palette.action.hover,
+  },
+}));
 
-    const customInputRanges = [
-    {
-      label: ' ',
-      range(value) {
-        return {
-          startDate: value,
-          endDate: value,
-        };
-      },
-    },
-  ];
-    const isBlocked = (day) => {
-        if (day <= moment().subtract(1, "days")) return true;
-        // if (
-        //   product.schedule.available.length &&
-        //   !product.schedule.available.some(
-        //     (e) => e.day === day.format("dddd").toLocaleLowerCase()
-        //   )
-        // )
-        //   return true;
+function CustomDateRangePicker() {
+  const [value, setValue] = useState([null, null]);
 
-        // if (blockedDays.some((date) => day.isSame(date, "day"))) return true;
+  const startDate = value[0];
+  const endDate = value[1];
 
-        // if (
-        //   bookedDays?.some(
-        //     (date) =>
-        //       (date.interval != PriceIntervalEnum.Minute ||
-        //         date.interval != PriceIntervalEnum.Hour) &&
-        //       day.isSameOrAfter(date.startDate, "day") &&
-        //       day.isSameOrBefore(date.endDate, "day")
-        //   )
-        // ) {
-        //   return true;
-        // }
-        // return false;
-    };
-
-    const customStyles = `
-        .rdrDayHovered {
-            background-color: rgba(0, 128, 0, 0.2) !important;
-            border-radius: 10 !important;
-        }
-        .rdrToday {
-            background-color: green !important;
-        }
-
-        .rdrInRange,
-        .rdrStartEdge,
-        .rdrEndEdge {
-          background-color: rgba(0, 128, 0, 0.5) !important;
-          border-radius: 10 !important;
-        }
-
-        .rdrSelected {
-            background-color: #91bf76 !important;
-            border-radius: 10 !important;
-        }
-        .rdrToday span {
-            border-bottom: 2px solid green !important;
-        }
-    `;
-
-    return (
-        <>
-            <style>{customStyles}</style>
-                <div>
-                    <DateRangePicker
-                        ranges={[dateRange]}
-                        onChange={(item) => setDateRange({ ...item.selection })}
-                        showDateDisplay={false}
-                        showMonthAndYearPickers={true}
-                        staticRanges={[]}
-                        inputRanges={[]}
-                        minDate={new Date(1900, 0, 1)} // Set the minimum selectable date
-                        maxDate={new Date(2099, 11, 31)} // Set the maximum selectable date
-                    />
-                </div>
-            </>
-    );
+  return (
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <StaticDateRangePicker
+        displayStaticWrapperAs="desktop"
+        value={value}
+        onChange={(newValue) => setValue(newValue)}
+        components={{
+          Day: StyledDateRangePickerDay,
+        }}
+        componentsProps={{
+          day: {
+            styleProps: {
+              selected: {
+                backgroundColor: 'red',
+                color: 'white',
+              },
+              selectedStartDate: {
+                borderTopLeftRadius: '50%',
+                borderBottomLeftRadius: '50%',
+              },
+              selectedEndDate: {
+                borderTopRightRadius: '50%',
+                borderBottomRightRadius: '50%',
+              },
+            },
+          },
+        }}
+      />
+    </LocalizationProvider>
+  );
 }
 
-export default DateRange;
+export default CustomDateRangePicker;
