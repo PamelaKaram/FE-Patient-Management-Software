bded6ea6978519e94453eee5f08912caff304566
PamelaKaram <pamela.karam01@lau.edu>
2023-04-29 18:45:17 +0300

    fixed current pres page

--- src/pages/doctor/patients/[uuid]/current-prescription.js
+++ src/pages/doctor/patients/[uuid]/current-prescription.js
@@ -1,24 +1,9 @@
-import { Button } from "@mui/material";
-import DownloadOutlinedIcon from "@mui/icons-material/DownloadOutlined";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 
 const CurrentPrecription = ({ patientData, medicines }) => {
   console.log(patientData, medicines);
   return (
     <main>
-      <Button
-        sx={{
-          backgroundColor: "green",
-          color: "white",
-          fontSize: "14px",
-          fontWeight: "bold",
-          padding: "10px 20px",
-        }}
-      >
-        <DownloadOutlinedIcon sx={{ mr: "10px" }} />
-        Pick a Date
-      </Button>
+
     </main>
   );
 };
