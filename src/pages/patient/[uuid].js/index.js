c147c775bbdd4c4fbdb0c89264f753aa9e53297a
Tracy Salloum <tracy.salloum@lau.edu>
2023-04-30 17:27:27 +0300

    patient page responsiveness adjustments

--- src/pages/patient/[uuid].js/index.js
+++ src/pages/patient/[uuid].js/index.js
@@ -21,49 +21,40 @@ export default function Patient({ data }) {
   };
   return (
     <div className={SideStyles.body}>
-      <NavBar />
-      <SideBar />
+      <NavBar className={SideStyles.navBarMobile} />
+      <SideBar className={`${SideStyles.sideBar} ${SideStyles.sideBarDesktop}`} />
       <div className={SideStyles.leftHalf}>
+        <NavBar className={SideStyles.navBarDesktop} />
         <div className={SideStyles.header}>
           <h1>Hi, Pamela Karam</h1>
-          <p className={SideStyles.text}>{"Let's track your health Today!"}</p>
+          <p className={SideStyles.text}>Let's track your health Today!</p>
         </div>
-        <div className={SideStyles.app}>
-          <h3>Upcoming Appointments</h3>
-          <AppDateTime />
+        <div className={SideStyles.topSection}>
+          <div className={SideStyles.app}>
+            <h3>Upcoming Appointments</h3>
+            <AppDateTime />
+          </div>
+          <div className={SideStyles.reminder}>
+            <h3>Daily Reminder!</h3>
+            <Reminders />
+          </div>
         </div>
         <div className={SideStyles.activities}>
           <h3>Activities</h3>
           <p className={SideStyles.text}>Today, 7 May 2023</p>
           <Buttons />
         </div>
-        <div className={SideStyles.bottom}>
-          <div className={SideStyles.reminder}>
-            <h3>Daily Reminder!</h3>
-            <Reminders />
+        <div className={SideStyles.calendarAndLinks}>
+          <div style={{ width: "100%", margin: "50px" }}>
+            <h3 className={SideStyles.calendarTitle}>Calendar</h3>
+            <DateRange />
           </div>
           <div className={SideStyles.link}>
-            <div>
-              <h3>Quick Links</h3>
-              <Links />
-            </div>
+            <h3>Quick Links</h3>
+            <Links />
           </div>
         </div>
-      </div>
-      <div style={{ width: "100%", margin: "50px" }}>
-        <LocalizationProvider dateAdapter={AdapterDateFns}>
-          <StaticDatePicker
-            value={date}
-            label="See Appointments"
-            onChange={handleSingleDateChange}
-
-            // initialVisibleMonth={() => moment()}
-            // focused={focus}
-            // numberOfMonths={1}
-            // onFocusChange={({ focused }) => setFocus(focused)}
-            // isDayBlocked={isBlocked}
-          />
-        </LocalizationProvider>
+        <div className={SideStyles.bottom}></div>
       </div>
     </div>
   );
