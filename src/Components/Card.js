01275fee02c5b229924f924af41fec3183e51018
PamelaKaram <pamela.karam01@lau.edu>
2023-05-01 22:25:41 +0300

    linking to cards of home on the same page

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import buttonStyles from '../styles/HomeCards.module.css';
-
-const Card = ({ title, buttonColor, buttonText }) => {
+const Card = ({ title, buttonColor, buttonText, onClick }) => {
   return (
-    <div className={`${buttonStyles.card} ${buttonStyles[buttonColor]}`}>
+    <div className={`${buttonStyles.card} ${buttonStyles[buttonColor]}`} onClick={onClick}>
       <h3>{title}</h3>
       <button className={`${buttonStyles.button} ${buttonStyles[buttonColor]}`}>
         {buttonText}
