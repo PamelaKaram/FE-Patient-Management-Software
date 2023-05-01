958e37989d88e14b04fd9e21cf9240a3be8eaa2a
PamelaKaram <pamela.karam01@lau.edu>
2023-05-01 16:35:52 +0300

    linking to popup in request apt general view

--- src/Components/RequestAptPop.js
+++ src/Components/RequestAptPop.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
-import { useForm } from 'react-hook-form';
-import SideStyles from '../styles/EmailReq.module.css';
-
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import SideStyles from "../styles/EmailReq.module.css";
+import axios from "../../lib/axios";
 
 function RequestAptPop() {
   const [isOpen, setIsOpen] = useState(false);
@@ -11,18 +11,26 @@ function RequestAptPop() {
     setIsOpen(!isOpen);
   };
 
-  const onSubmit = (data) => {
-    console.log(data); // you can do something with the data here, e.g. send it to the server
+  const onSubmit = async (data) => {
+    const { firstName, lastName, email, phone, message } = data;
+    try {
+      await axios.post("requests/", {
+        firstName,
+        lastName,
+        email,
+        phoneNumber: phone,
+        message,
+      });
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
     <div>
       <button className={SideStyles.customButton} onClick={togglePopup}>
-        <span className={SideStyles.text}>
-          Request an Appointment
-        </span>
+        <span className={SideStyles.text}>Request an Appointment</span>
       </button>
-
 
       {isOpen && (
         <div className={SideStyles.popupInner}>
@@ -34,17 +42,39 @@ function RequestAptPop() {
           </div>
           <div className={SideStyles.info}>
             <form onSubmit={handleSubmit(onSubmit)}>
-              <label>Name:</label>
-              <input type="text" {...register("name")} placeholder="Enter your name" />
+              <label>First Name:</label>
+              <input
+                type="text"
+                {...register("firstName")}
+                placeholder="Enter your first name"
+              />
+
+              <label>Last Name:</label>
+              <input
+                type="text"
+                {...register("lastName")}
+                placeholder="Enter your last name"
+              />
 
               <label>Email:</label>
-              <input type="email" {...register("email")} placeholder="Enter your email" />
+              <input
+                type="email"
+                {...register("email")}
+                placeholder="Enter your email"
+              />
 
               <label>Phone:</label>
-              <input type="tel" {...register("phone")} placeholder="Enter your phone number" />
+              <input
+                type="tel"
+                {...register("phone")}
+                placeholder="Enter your phone number"
+              />
 
               <label>Message:</label>
-              <textarea {...register("message")} placeholder="Enter your message"></textarea>
+              <textarea
+                {...register("message")}
+                placeholder="Enter your message"
+              ></textarea>
 
               <button type="submit">Submit</button>
             </form>
